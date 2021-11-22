d0c693e9a9d5b9ef793bb508900e5ce1c90f2d1c
dammonmarrod <68920289+dammonmarrod@users.noreply.github.com>
2021-11-22 04:30:51 +0100

    Update Moneda.java

--- primer_trimestre/practicas/actividad2/Moneda.java
+++ primer_trimestre/practicas/actividad2/Moneda.java
@@ -1,59 +1,140 @@
-package com.example.aplicacion5_.helpers;
+package com.example.aplicacion5_.models;
 
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.util.LruCache;
+import android.util.Log;
 
 //Importamos Volley
 import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.ImageLoader;
-import com.android.volley.toolbox.Volley;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 
-public class QueueUtils {
-    private static QueueObject uniqueInstance;
-    public static class QueueObject {
-        private RequestQueue mRequestQueue;
-        private ImageLoader mImageLoader; //Si desde el JSON nos llegarán también imagenes
-        private static Context mCtx;
-        private QueueObject(Context context) {
-            mCtx = context;
-            mRequestQueue = getRequestQueue();
-            mImageLoader = new ImageLoader(mRequestQueue,
-                    new ImageLoader.ImageCache() {
-                        private final LruCache<String, Bitmap>
-                                cache = new LruCache<String, Bitmap>(20);
-                        @Override
-                        public Bitmap getBitmap(String url) {
-                            return cache.get(url);
-                        }
-                        @Override
-                        public void putBitmap(String url, Bitmap bitmap) {
-                            cache.put(url, bitmap);
-                        }
-                    });
-        }
+//Importamos nuestras clases que vamos a utilizar
+import com.example.aplicacion5_.MainActivity;
+import com.example.aplicacion5_.helpers.QueueUtils;
 
-        public RequestQueue getRequestQueue() {
-            if (mRequestQueue == null) {
-                mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
-            }
-            return mRequestQueue;
-        }
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-        public <T> void addToRequestQueue(Request<T> req) {
-            getRequestQueue().add(req);
-        }
+import java.util.ArrayList;
 
-        public ImageLoader getImageLoader() {
-            return mImageLoader;
-        }
+public class Moneda {
+    //Variables que vamos a utilizar del JSON
+    public String code;
+    public String name;
+
+    public String rate;
+    // rate    1 euro  >  1,1282142 Dólares estadounidenses
+    public String inverseRate;
+    //inverseRate   1 USD   >  0,88636466 Euros
+
+    //Constructor
+    public Moneda(String _code, String _name, String _rate, String _inverseRate) {
+        this.code = _code;
+        this.name = _name;
+        this.rate = _rate;
+        this.inverseRate = _inverseRate;
     }
 
-    public static synchronized QueueObject getInstance(Context context) {
-        if (uniqueInstance == null) {
-            uniqueInstance = new QueueObject(context);
-        }
-        return uniqueInstance;
+    //Creamos los Getters para acceder y los Setters para modificar la clase Moneda
+    public String getCode() {
+        return code;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRate() {
+        return rate;
+    }
+
+    public String getInverseRate() {
+        return inverseRate;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRate(String rate) {
+        this.rate = rate;
+    }
+
+    public void setInverseRate(String inverseRate) {
+        this.inverseRate = inverseRate;
+    }
+
+
+
+    //SI LOS DATOS LOS CREAMOS NOSOTROS DENTRO DE LA APP
+    public static ArrayList getCollection() {
+        ArrayList<Moneda> collection = new ArrayList<>();
+        collection.add(new Moneda("USD", "U.S. Dollar", "1.1300941915987", "0.88488199252251"));
+        collection.add(new Moneda("GBP", "U.K. Pound Sterling","0.84045383327365","1.1898333500424"));
+        return collection;
+    }
+
+
+
+    //SI TRABAJAMOS CON EL ARCHIVO JSON
+    public static void injectContactsFromCloud(
+            final QueueUtils.QueueObject o,
+            final ArrayList<Moneda> monedas,
+            final MainActivity _interface) {
+        //Accedemos a los datos JSON
+        String url = "https://api.jsonbin.io/b/619af69862ed886f9152b718/2";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //Si el primer objeto se llama teams
+                        if (response.has("moneda")) {
+                            try {
+                                //Accedemos al array de Objetos
+                                JSONArray monedasObtendias = response.getJSONArray("moneda");
+
+                                //Debug para verificar que nos llegan los datos
+                                String longitudJsonArray= "" + (monedasObtendias.length());
+                                Log.v("respuesta", " Longitud JSonArray " + longitudJsonArray);
+
+                                for (int i = 0; i < monedasObtendias.length(); i++) {
+
+                                    JSONObject datosMoneda = monedasObtendias.getJSONObject(i);
+                                    String code =  datosMoneda.getString("code");
+                                    String name = datosMoneda.getString("name");
+                                    String rate = datosMoneda.getString("rate");
+                                    String inverseRate = datosMoneda.getString("inverseRate");
+
+                                    Log.v("datos", code + "- " + name + " -" + rate + "-" + inverseRate);
+                                    //Añadimos los datos al final ArrayList<Moneda> monedas;
+                                    monedas.add(new Moneda(code,name, rate, inverseRate));
+                                }
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                                Log.v("respuesta", "Error al Acceder a los JSOM");
+                            }
+                            // Esta función se implementa en el MainActivity
+                            _interface.refreshList();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.v("respuesta", "Error Volley");
+                    }
+                });
+
+        o.addToRequestQueue(jsonObjectRequest);
+    }
+
+
 }
